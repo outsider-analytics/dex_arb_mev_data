9a64966d7c788b8f00844782678a63d4c138cfd9
outsider-analytics <105951311+outsider-analytics@users.noreply.github.com>
2022-10-31 23:15:41 -0600

    Update mev_data_explorer.sql

--- mev_data_explorer.sql
+++ mev_data_explorer.sql
@@ -1,7 +1,3 @@
-
-
-
-
 WITH miner_MEV as (
   SELECT
     *,
@@ -9,30 +5,31 @@ WITH miner_MEV as (
   FROM `starlit-sandbox-349500.Data.MEV_FINDER_RESULTS_2` 
   UNION ALL  
   SELECT
-    b.miner as acct_address,
+    MAX(b.miner) as acct_address,
     transaction_hash,
     "ether" as token_address,
-    (gas_price - b.base_fee_per_gas) * (receipt_gas_used/1e18) as net_flows,
-    from_address,
-    0 as num_outs,
+    MAX((gas_price - b.base_fee_per_gas) * (receipt_gas_used/1e18)) as net_flows,
+    MAX(from_address),
+    1 as num_outs, --I'll fudge this to keep it in
     0 neg_check,
-    num_transfers,
-    block_number,
-    block_timestamp,
-    tx_gas_costs,
-    gas_price,
-    receipt_gas_used,
+    AVG(num_transfers),
+    MAX(block_number),
+    MAX(block_timestamp),
+    MAX(tx_gas_costs),
+    MAX(gas_price),
+    MAX(receipt_gas_used),
     1 as miner
   FROM `starlit-sandbox-349500.Data.MEV_FINDER_RESULTS_2` r
   LEFT JOIN `bigquery-public-data.crypto_ethereum.blocks` b
     ON b.number = r.block_number
   WHERE r.acct_address != '0x030ba81f1c18d280636f32af80b9aad02cf0854e'
+  GROUP BY transaction_hash
 ),
 
 separate_miner as (
   SELECT 
     *,
-    MAX(miner) OVER (PARTITION BY acct_address, transaction_hash) as miner_check
+    SUM(miner) OVER (PARTITION BY acct_address, transaction_hash)
   FROM miner_MEV
 ),
 
@@ -50,24 +47,16 @@ merge_check as (
   WHERE acct_address != '0x030ba81f1c18d280636f32af80b9aad02cf0854e'
   -- AND acct_address != '0x05656db19ec9ff8dfb437475b3d76ca9a29e968f'
   -- GROUP BY acct_address, token_address
+  AND num_outs > 0
   ORDER BY net_flows desc)
 
--- FOR pre/post merge chart
--- SELECT 
---   pre_merge,
---   day_diff,
---   COUNT(DISTINCT acct_address) AS MEV_Distinct_Participants,
---   COUNT(*) AS num_MEV,
---   SUM(net_flows) AS total_MEV,
---   SUM(net_flows)/COUNT(*) as MEV_per_opportunity
--- FROM merge_check
--- GROUP BY pre_merge, day_diff
--- ORDER BY day_diff, pre_merge
-
---To View Miner Share of MEV
-SELECT
+SELECT 
   pre_merge,
-  miner_check,
-  SUM(net_flows)
+  day_diff,
+  COUNT(DISTINCT acct_address) AS MEV_Distinct_Participants,
+  COUNT(*) AS num_MEV,
+  SUM(net_flows) AS total_MEV,
+  SUM(net_flows)/COUNT(*) as MEV_per_opportunity
 FROM merge_check
-GROUP BY pre_merge, miner_check
+GROUP BY pre_merge, day_diff
+ORDER BY day_diff, pre_merge
